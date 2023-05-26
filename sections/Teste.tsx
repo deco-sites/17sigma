11a87eccf86e5bc5d47a5e3262ac1d2f40b625c1
Marcos Antonio Soares <marcosas.soares2@gmail.com>
2023-05-26 08:56:57 -0300

    Update Teste.tsx

--- sections/Teste.tsx
+++ sections/Teste.tsx
@@ -1,5 +1,5 @@
 export default function Teste() {
   return (
-  <div class='h-screen w-full bg-primaryteste'><h1 class='text-withe'>Teste</h1></div>
+  <div class='h-screen bg-primaryteste'><h1 class='text-withe'>Teste</h1></div>
   );
 }
