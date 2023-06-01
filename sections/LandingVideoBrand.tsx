100de5f2a50bd2489532977e2102e27abc3ebf6b
mrfullstack <marcosfullstack@gmail.com>
2023-05-31 21:04:42 -0300

    corrigindo hiddenOnScroll

--- sections/LandingVideoBrand.tsx
+++ sections/LandingVideoBrand.tsx
@@ -9,7 +9,12 @@ export default function LandingPatners({
   videoSource,
 }: LandingPatnersProps) {
   return (
-    <video controls playsInline preload="auto"  className="h-full w-full object-cover"> 
+    <video
+      controls
+      playsInline
+      preload="auto"
+      className="h-full w-full object-cover"
+    >
       <source
         src={videoSource ? videoSource : asset(`/brand.webm`)}
         type="video/webm"
