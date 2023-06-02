a9a51c8135bb3662f55f99560e4cc8a9e256819c
João <joaopfc2764@gmail.com>
2023-06-01 21:51:11 -0300

    create footer

--- sections/LandingFooter.tsx
+++ sections/LandingFooter.tsx
@@ -1,12 +1,29 @@
+import { FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import Container from "../components/Container.tsx";
+
 export default function LandingFooter() {
   return (
     <footer class="h-[7rem] bg-landing-section">
-      <p>Ícone</p>
+      <Container>
+        <div class="flex">
+          <div class="h-[7rem] w-[7rem] mr-[10rem]">
+            <img src="../iso.png" alt="" class="h-[100%] w-[100%]"/>
+          </div>
 
-      <p>All Rights Reserved © 2022 — 17Sigma</p>
+          <div class="h-12 w-2/4 mt-8 mr-[10rem] flex items-center justify-center">
+            <p class="text-white text-[0.7rem]">All Rights Reserved © 2022 — 17Sigma</p>
+          </div>
 
-      <a href="#">Twitter</a>
-      <a href="#">LinkedIn</a>
+          <div class="h-11 w-20 mt-8 flex items-center justify-center">
+            <a href="#" class="mr-4">
+              <FaTwitter size={24} className="text-landing-primary" />
+            </a>
+            <a href="#">
+              <FaLinkedinIn size={24} className="text-landing-primary" />
+            </a>
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 }
