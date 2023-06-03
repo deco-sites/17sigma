5831fb60f50707263a172ca4f4ced45b82488a7e
João <joaopfc2764@gmail.com>
2023-06-02 23:29:41 -0300

    fix footer css

--- sections/LandingForm.tsx
+++ sections/LandingForm.tsx
@@ -4,7 +4,7 @@ import Container from "../components/Container.tsx";
 export default function LandingForm() {
   return (
     <>
-      <div class="h-[28rem] bg-landing-background border-b-transparent">
+      <div class="container-xl position-relative bg-landing-background">
         <Container>
           <h1 class="mb-5 pt-20 text-[2.5rem] text-white font-bold">
             We would love to hear from you
@@ -12,9 +12,9 @@ export default function LandingForm() {
 
           <div class="flex flex-row">
             <div class="h-[18rem] w-[70%]">
-              <form action="" class="h-64 mt-1 ml-10 flex flex-col">
+              <form action="" class="h-64 mt-1 flex flex-col gap-4">
                 <input
-                  class="ml-14 w-[75.5%] border-b border-b-landing-primary bg-landing-background pb-2 pt-6 text-landing-primary placeholder-landing-primary focus:outline-none"
+                  class="w-[75.5%] border-b border-b-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none"
                   type="text"
                   name="fullName"
                   id="fullNameId"
@@ -22,16 +22,16 @@ export default function LandingForm() {
                 />
 
                 <input
-                  class="ml-14 w-[75.5%] border-b border-b-landing-primary bg-landing-background pb-2 pt-6 text-landing-primary placeholder-landing-primary focus:outline-none"
+                  class="w-[75.5%] border-b border-b-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none"
                   type="email"
                   name="email"
                   id="emailId"
                   placeholder="EMAIL *"
                 />
 
-                <div class="h-24 w-[95%] ml-14 flex flex-row">
+                <div class="h-24 w-[95%] flex flex-row">
                   <textarea
-                    class="h-24 w-full border-b border-landing-primary bg-landing-background pt-8 pb-2 text-landing-primary placeholder-landing-primary focus:outline-none"
+                    class="h-24 w-full border-b border-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none"
                     name="textarea"
                     id="textareaId"
                     placeholder="SAY SOMETHING *"
@@ -56,6 +56,11 @@ export default function LandingForm() {
             </div>
           </div>
         </Container>
+      </div>
+      <div class="row flex">
+        <div class="col-md-9">
+          <p class="mailproblem"></p>
+        </div>
       </div>
       <ButtonForm />
     </>
