2932e95d84fd9d4d337e8504249ccff0a368d243
João <joaopfc2764@gmail.com>
2023-06-03 14:52:36 -0300

    update form and footer

--- sections/LandingFooter.tsx
+++ sections/LandingFooter.tsx
@@ -2,7 +2,23 @@ import { FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { asset } from "$fresh/runtime.ts";
 import Container from "../components/Container.tsx";
 
-export default function LandingFooter() {
+interface Props {
+  logo?: string
+  first_icon?: string
+  first_url?: string
+  second_icon?: string
+  second_url?: string
+  footer_text?: string
+}
+
+export default function LandingFooter({
+  logo,
+  first_icon,
+  first_url,
+  second_icon,
+  second_url,
+  footer_text
+}: Props) {
   return (
     <footer class="h-[5rem] bg-landing-section flex justify-center items-center">
       <Container>
@@ -10,23 +26,33 @@ export default function LandingFooter() {
           <div class="h-[5rem] w-[100%] row flex items-center justify-center">
             <div class="w-1/3">
               <a href="#">
-                <img src={asset("/iso.png")} alt="17 Sigma" />
+                <img src={logo ? logo : asset("/iso.png")} alt="17 Sigma" />
               </a>
             </div>
 
             <div class="w-1/3 text-center items-center">
               <a class="text-white text-[0.6rem]">
-                All Rights Reserved © 2022 — 17Sigma
+                {
+                  footer_text ? footer_text : "All Rights Reserved © 2022 — 17Sigma"
+                }
               </a>
             </div>
 
             <div class="w-1/3 flex justify-end">
               <ul class="flex items-center justify-center">
-                <a href="#" class="mr-4">
-                  <FaTwitter size={24} className="text-landing-primary" />
+                <a href={first_url ? first_url : "#"} class="mr-4">
+                  {
+                    first_icon 
+                      ? first_icon 
+                      : <FaTwitter size={24} className="text-landing-primary" />
+                  }
                 </a>
-                <a href="#">
-                  <FaLinkedinIn size={24} className="text-landing-primary" />
+                <a href={second_url ? second_url : "#"}>
+                  {
+                    second_icon
+                      ? second_icon
+                      : <FaLinkedinIn size={24} className="text-landing-primary" />
+                  }
                 </a>
               </ul>
             </div>
--- sections/LandingForm.tsx
+++ sections/LandingForm.tsx
@@ -1,63 +1,105 @@
 import ButtonForm from "../components/ButtonForm.tsx";
 import Container from "../components/Container.tsx";
+import { AiOutlineArrowRight } from 'react-icons/ai'
 
-export default function LandingForm() {
+interface Props {
+  title?: string
+  first_input_name?: string
+  first_input_id?: string
+  first_input_placeholder?: string
+  second_input_name?: string
+  second_input_id?: string
+  second_input_placeholder?: string
+  thirsty_input_name?: string
+  thirsty_input_id?: string
+  thirsty_input_placeholder?: string
+  attachment_button_text?: string
+  submit_button_icon?: string
+  submit_button_text?: string
+}
+
+export default function LandingForm({
+  attachment_button_text,
+  first_input_id,
+  first_input_name,
+  first_input_placeholder,
+  second_input_id,
+  second_input_name,
+  second_input_placeholder,
+  thirsty_input_id,
+  thirsty_input_name,
+  thirsty_input_placeholder,
+  submit_button_icon,
+  submit_button_text,
+  title,
+}: Props) {
   return (
     <>
-      <div class="container-xl position-relative bg-landing-background">
-        <Container>
-          <h1 class="mb-5 pt-20 text-[2.5rem] text-white font-bold">
-            We would love to hear from you
-          </h1>
+      <form>
+        <div class="container-xl position-relative bg-landing-background">
+          <Container>
+            <div class="h-[10rem] flex items-center py-4">
+              <h3 class="text-white font-bold text-[2.5rem]">
+                {
+                  title
+                    ? title
+                    : "We would love to hear from you"
+                }
+              </h3>
+            </div>
 
-          <div class="flex flex-wrap">
-            <div class="h-[18rem] w-full md:w-[70%]">
-              <form action="" class="h-64 mt-1 flex flex-col gap-4">
+            <div class="flex flex-wrap mt-5 pb-4">
+              <div class="h-[14rem] w-full md:w-[75%] flex flex-col justify-between">
                 <input
-                  class="w-[75.5%] border-b border-b-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none"
+                  class="w-[75.5%] border-b border-b-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none pt-1 pb-1"
                   type="text"
-                  name="fullName"
-                  id="fullNameId"
-                  placeholder="FULL NAME *"
+                  name={first_input_name ? first_input_name : "fullName"}
+                  id={first_input_id ? first_input_id : "fullNameId"}
+                  placeholder={first_input_placeholder ? first_input_placeholder : "FULL NAME *"}
+                  required
                 />
 
                 <input
-                  class="w-[75.5%] border-b border-b-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none"
+                  class="w-[75.5%] border-b border-b-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none pb-1"
                   type="email"
-                  name="email"
-                  id="emailId"
-                  placeholder="EMAIL *"
+                  name={second_input_name ? second_input_name : "email"}
+                  id={second_input_id ? second_input_id: "emailId"}
+                  placeholder={second_input_placeholder ? second_input_placeholder : "EMAIL *"}
+                  required
                 />
 
-                <div class="h-24 w-[95%] flex flex-row">
+                <div class="h-[6rem] w-[100%] flex flex-row">
                   <textarea
-                    class="h-24 w-full border-b border-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none"
-                    name="textarea"
-                    id="textareaId"
-                    placeholder="SAY SOMETHING *"
+                    class="h-[4rem] w-full border-b border-landing-primary bg-landing-background text-landing-primary placeholder-landing-primary focus:outline-none"
+                    name={thirsty_input_name ? thirsty_input_name : "textarea"}
+                    id={thirsty_input_id ? thirsty_input_id : "textareaId"}
+                    placeholder={thirsty_input_placeholder ? thirsty_input_placeholder : "SAY SOMETHING *"}
                   >
                   </textarea>
                 </div>
-              </form>
-            </div>
+              </div>
 
-            <div class="h-40 flex flex-col mt-[5rem] ml-8">
-              <input
-                class="mb-7 h-14 w-60 rounded-full border border-landing-primary bg-landing-background text-landing-primary text-lg cursor-pointer"
-                type="button"
-                value="+ ATTACHMENT"
-              />
+              <div class="h-[9rem] flex flex-col mt-[3.5rem] ml-5">
+                <label class="h-14 w-60 mb-7 rounded-full border border-landing-primary bg-landing-background text-landing-primary text-lg flex items-center justify-center cursor-pointer">
+                  {
+                    attachment_button_text
+                      ? attachment_button_text
+                      : "+ ATTACHMENT"
+                  }
+                  <input type="file" name="file" id="fileId" class="hidden"/>
+                </label>
 
-              <input
-                class="h-14 w-60 rounded-full text-white text-lg bg-landing-primary cursor-pointer"
-                type="button"
-                value="-> GET IN TOUCH"
-              />
+                <button type="submit" class="h-14 w-60 rounded-full text-white text-lg bg-landing-primary flex items-center justify-center cursor-pointer gap-1">
+                  { submit_button_icon ? submit_button_icon : <AiOutlineArrowRight size={20} /> }
+                  { submit_button_text ? submit_button_text : "GET IN TOUCH" }
+                </button>
+              </div>
             </div>
-          </div>
-        </Container>
-      </div>
-      <div class="row flex">
+          </Container>
+        </div>
+      </form>
+      
+      <div class="h-[1rem] flex bg-landing-background">
         <div class="col-md-9">
           <p class="mailproblem"></p>
         </div>
