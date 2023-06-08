5a9ad04ad7e78d3ac7aed81df7bb763bd24c42ee
mrfullstack <marcosfullstack@gmail.com>
2023-06-08 20:19:27 -0300

    correção scroll link seção video brand

--- sections/LandingPatners.tsx
+++ sections/LandingPatners.tsx
@@ -12,20 +12,20 @@ export default function LandingPatners(props: Props) {
     return (
       <>
         <a name="what"></a>
-        <div  className="bg-landing-background pt-20 pb-16">
-        <Container>
-          <h3 class="animate-on-scroll text-[1.3125rem] text-landing-primary mb-2 uppercase">
-            What
-          </h3>
-          <p class="animate-on-scroll animate-delay-200 text-[2.01rem] text-white font-medium mb-4">
-            We partner with early stage LatAm startups.
-          </p>
-          <p className="animate-on-scroll animate-delay-300 text-[1.3rem] text-white mb-4">
-            Pre-Seed | Seed
-          </p>
-        </Container>
-        <FilterPartners partners={props.partners} />
-      </div>
+        <div className="bg-landing-background pt-20 pb-16">
+          <Container>
+            <h3 class="animate-on-scroll text-[1.3125rem] text-landing-primary mb-2 uppercase">
+              What
+            </h3>
+            <p class="animate-on-scroll animate-delay-200 text-[2.01rem] text-white font-medium mb-4">
+              We partner with early stage LatAm startups.
+            </p>
+            <p className="animate-on-scroll animate-delay-300 text-[1.3rem] text-white mb-4">
+              Pre-Seed | Seed
+            </p>
+          </Container>
+          <FilterPartners partners={props.partners} />
+        </div>
       </>
     );
   }
