3ba244d8dac48a7c2a78bf5fc84e9307fe8d3a68
mrfullstack <marcosfullstack@gmail.com>
2023-06-08 21:38:39 -0300

    finalizando loading inicial

--- islands/LandingLoading.tsx
+++ islands/LandingLoading.tsx
@@ -1,48 +1,57 @@
-import { useEffect,useState,useCallback } from "preact/hooks";
+import { useCallback, useEffect, useState } from "preact/hooks";
 import { LogoAnimate } from "../components/IconSvg.tsx";
 
 interface LandingLoadingProps {
-  loading:boolean;
+  loading: boolean;
 }
 
-export default function LandingLoading({loading}:LandingLoadingProps) {
+export default function LandingLoading({ loading }: LandingLoadingProps) {
+  const [start, setStart] = useState(false);
+  const [close, setClose] = useState(false);
 
-  const [ start, setStart ] = useState(false);
-  const [ close, setClose ] = useState(false);
-
-  useEffect(()=>{
-    
-    if(!loading)
-    {
-      const timeout = setTimeout(()=>{
+  useEffect(() => {
+    if (!loading) {
+      const timeout = setTimeout(() => {
         setClose(true);
         clearInterval(timeout);
-      },1200)
+      }, 1000);
     }
+  }, [loading]);
 
-  },[loading])
 
 
-  useEffect(()=>{
-    // console.log(new Date().getSeconds())
-    const interval = setInterval( () => {
-      setStart(!start);
-    },600)
+  useEffect(() => {
 
-    return(()=> { 
-      clearInterval(interval)
-    })
+      const startInterval = () => setInterval(() => {
+        setStart(!start);
+      }, 600);
 
-  },[start])
+      const interval = startInterval();
+
+      if(!loading)
+      {
+        return (() => {
+          clearInterval(interval);
+        });
+      }
+
+      return (() => {
+        clearInterval(interval);
+      });
+      
+  }, [loading,start]);
 
   return (
-    <div id="loading" className={ ( close ? 'closing' : '') + ' pointer-events-none fixed h-screen w-screen z-50 flex items-center justify-center'}>
-      <div className='loading-door z-0 w-[50%] h-full absolute top-0 left-0 bg-landing-background'></div>
-      <div className='loading-door z-0 w-[50%] h-full absolute top-0 right-0 bg-landing-background'></div>
-      <div className='z-10'>
-        <LogoAnimate start={start}/>
+    <div
+      id="loading"
+      className={(close ? "closing" : "") +
+        " pointer-events-none fixed h-screen w-screen z-50 flex items-center justify-center"}
+    >
+      <div className="loading-door z-0 w-[50%] h-full absolute top-0 left-0 bg-landing-background">
       </div>
+      <div className="loading-door z-0 w-[50%] h-full absolute top-0 right-0 bg-landing-background">
+      </div>
+      <LogoAnimate start={start} />
     </div>
-  )
-  
+  );
 }
